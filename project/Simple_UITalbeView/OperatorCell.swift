b1e541c7f774b9e4e04f29a388c786e71b010898
Hajime Taniguchi <hameji@gmail.com>
2021-12-07 23:32:37 -0500

    CustomTableViewCellを作成

---
+++ project/Simple_UITalbeView/OperatorCell.swift
@@ -0,0 +1,23 @@
+//
+//  OperatorCell.swift
+//  Simple_UITalbeView
+//
+//  Created by Hajime Taniguchi on 2021/12/07.
+//
+
+import UIKit
+
+class OperatorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
