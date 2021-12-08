298fdc3d72f3d397880e9f8e0db801af2b3d870c
Hajime Taniguchi <hameji@gmail.com>
2021-12-07 22:35:11 -0500

    Xcodeでプロジェクトを作成しファイル名を変更

---
+++ project/Simple_UITalbeView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple_UITalbeView
+//
+//  Created by Hajime Taniguchi on 2021/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
