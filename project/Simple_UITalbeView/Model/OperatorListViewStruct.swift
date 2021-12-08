ccd4d8e78c0fdc6d88a58afbe9747cd456b8766f
Hajime Taniguchi <hameji@gmail.com>
2021-12-08 04:16:35 -0500

    StructでTableViewの情報まとめた

---
+++ project/Simple_UITalbeView/Model/OperatorListViewStruct.swift
@@ -0,0 +1,14 @@
+//
+//  OperatorListStruct.swift
+//  Simple_UITalbeView
+//
+//  Created by Hajime Taniguchi on 2021/12/08.
+//
+
+import Foundation
+
+struct OperatorListViewStruct {
+    var section: [OperatorType] {
+        return OperatorType.allCases
+    }
+}
