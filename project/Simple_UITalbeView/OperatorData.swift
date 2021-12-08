557bd2f539b1305b8fd387d3aff343ac9a8db9d9
Hajime Taniguchi <hameji@gmail.com>
2021-12-08 00:32:37 -0500

    enum作成

---
+++ project/Simple_UITalbeView/OperatorData.swift
@@ -0,0 +1,22 @@
+//
+//  OperatorData.swift
+//  Simple_UITalbeView
+//
+//  Created by Hajime Taniguchi on 2021/12/07.
+//
+
+import Foundation
+
+enum OperatorType {
+    case creating
+    case transforming
+    case filtering
+    case combining
+    case errorHandling
+    case utility
+    case conditionalAndBoolean
+    case mathematicalAndAggregate
+    case backpressure
+    case connectable
+    case convert
+}
