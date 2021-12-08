d7d5b1fca89c3679b5d89577560d5c1b6f787dfa
Hajime Taniguchi <hameji@gmail.com>
2021-12-08 01:20:55 -0500

    WKWebViewを配置し、ViewControllerに接続

--- project/Simple_UITalbeView/DetailViewController.swift
+++ project/Simple_UITalbeView/DetailViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
