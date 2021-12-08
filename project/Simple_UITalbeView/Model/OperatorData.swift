d3adcc4c9f2dd56affd0c0c1858652a91c5e30b9
Hajime Taniguchi <hameji@gmail.com>
2021-12-08 04:14:39 -0500

    operatorTypeを修正

--- project/Simple_UITalbeView/Model/OperatorData.swift
+++ project/Simple_UITalbeView/Model/OperatorData.swift
@@ -7,18 +7,34 @@
 
 import Foundation
 
-enum OperatorType {
-    case creating
-    case transforming
-    case filtering
-    case combining
-    case errorHandling
-    case utility
-    case conditionalAndBoolean
-    case mathematicalAndAggregate
-    case backpressure
-    case connectable
-    case convert
+enum OperatorType: Int, CaseIterable {
+    case creating = 0
+    case transforming = 1
+    case filtering = 2
+    case combining = 3
+    case errorHandling = 4
+    case utility = 5
+    case conditionalAndBoolean = 6
+    case mathematicalAndAggregate = 7
+    case backpressure = 8
+    case connectable = 9
+    case convert = 10
+    
+    var title: String {
+        switch self {
+        case .creating: return "Creatign Observables"
+        case .transforming: return "Transforming Observables"
+        case .filtering: return "Filtering Observables"
+        case .combining: return "Combining Observables"
+        case .errorHandling: return "Error Handling Operators"
+        case .utility: return "Observable Utility Operators"
+        case .conditionalAndBoolean: return "Conditional and Boolean Operators"
+        case .mathematicalAndAggregate: return "Mathematical and Aggregate Operators"
+        case .backpressure: return "Backpressure Operators"
+        case .connectable: return "ConnectableObservable Operators"
+        case .convert: return "Operators to Convert Observables"
+        }
+    }
 }
 
 struct Operator {
