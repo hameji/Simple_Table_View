cd60499d70bc79b625173c33f69dbf3136c7e21c
Hajime Taniguchi <hameji@gmail.com>
2021-12-08 22:18:52 -0500

    quizViewControllerを作成

---
+++ project/Simple_UITalbeView/ViewController/QuizViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuizViewController.swift
+//  Simple_UITalbeView
+//
+//  Created by Hajime Taniguchi on 2021/12/08.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+}
