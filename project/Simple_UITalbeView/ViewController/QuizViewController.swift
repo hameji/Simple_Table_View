00847655b993f18741248925cab1ca6677c749fe
Hajime Taniguchi <hameji@gmail.com>
2021-12-08 22:39:10 -0500

    outletをviewControllerに接続

--- project/Simple_UITalbeView/ViewController/QuizViewController.swift
+++ project/Simple_UITalbeView/ViewController/QuizViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-
+    
+    @IBOutlet weak var stream1ImageView: UIImageView!
+    @IBOutlet weak var stream2ImageView: UIImageView!
+    @IBOutlet weak var operatorImageView: UIImageView!
+    @IBOutlet weak var resultImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
